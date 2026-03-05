aa5b70e411c3b84de1f7b1cb421ffa332a251836
takudzwa <takchiks@gmail.com>
2026-03-05 17:42:44 -0500

    [getImportance && RottingOranges ] comments

--- EmployeeImportance.java
+++ EmployeeImportance.java
@@ -1,17 +1,22 @@
 
 class Solution {
+    // TimeComplexity: O(N)
+    // SpaceComplexity: O(N)
+    // Ran succesfully on Leetcode: yes
     public int getImportance(List<Employee> employees, int id) {
         int total = 0 ;
         HashMap<Integer, Employee> map = new HashMap<>();
         Queue<Employee> q = new LinkedList<>();
+        // map add all ids to respctive Employee object
         for (int i = 0; i < employees.size(); i++) {
             map.put(employees.get(i).id, employees.get(i));
         }
         q.add(map.get(id));
-
+        // iterate till they are no more subordinates linked to employee
         while(!q.isEmpty()) {
             Employee curr = q.poll();
             total = curr.importance + total;
+            // add curr empl subordinates to the queue
             for (int sub:curr.subordinates) {
                 q.add(map.get(sub));
             }
